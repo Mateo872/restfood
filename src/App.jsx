b74c816ea3431b733cf4f619c6b03a7fdebe0717
Mario-Chavez <marioachavez1@gmail.com>
2023-07-13 01:04:52 -0300

    acepte cambios entrantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./componentes/comunes/Header";
-<<<<<<< HEAD
 
 import QuienesSomos from "./componentes/comunes/QuienesSomos";
 
 import BuscadorPlatos from "../BuscadorPlatos";
+import Footer from "./componentes/comunes/Footer";
 
 function App() {
     return (
@@ -15,28 +15,9 @@ function App() {
                 <BuscadorPlatos />
                 <QuienesSomos />
             </main>
+            <Footer />
         </>
     );
-=======
-
-import QuienesSomos from "./componentes/comunes/QuienesSomos";
-
-import BuscadorPlatos from "../BuscadorPlatos";
-import Footer from "./componentes/comunes/Footer";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <BuscadorPlatos />
-    <QuienesSomos />
-      </main>
-      <Footer />
-    </>
-  );
-
->>>>>>> 0f284f68873f30d0fc81dcb3e115dea1ab83c786
 }
 
 export default App;
