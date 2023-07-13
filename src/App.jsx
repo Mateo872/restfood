e54bb8690d8684bbc47a1450f68a6407bae80d07
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-13 16:34:50 -0300

    Corrección

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./componentes/comunes/Header";
-
 import QuienesSomos from "./componentes/comunes/QuienesSomos";
-
 import BuscadorPlatos from "../BuscadorPlatos";
 import Footer from "./componentes/comunes/Footer";
 import Menu from "./componentes/comunes/Menu";
-import ContenedorCarrito from "./componentes/ContenedorCarrito";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        {/* <BuscadorPlatos /> */}
-        {/* <QuienesSomos /> */}
-        {/* <Menu /> */}
-        <ContenedorCarrito />
+        <BuscadorPlatos />
+        <QuienesSomos />
+        <Menu />
       </main>
       <Footer />
     </>
