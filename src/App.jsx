2dd7e2f50a9b7d51432b13fe61e2ec285cd00949
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-07-13 12:42:57 -0300

    Cree la carpeta pages, movi los componentes que eran llamados en App.jsx a Inicio.jsx, tambien cree BrowserRouter en app para llamar desde ahi a la pagina de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Header from "./componentes/comunes/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Administrador from "./pages/Administrador";
 
-import QuienesSomos from "./componentes/comunes/QuienesSomos";
-
-import BuscadorPlatos from "../BuscadorPlatos";
-import Footer from "./componentes/comunes/Footer";
-import Menu from "./componentes/comunes/Menu";
-
-function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <BuscadorPlatos />
-                <QuienesSomos />
-                <Menu />
-            </main>
-            <Footer />
-        </>
-    );
+function App() {    
+  return(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Inicio></Inicio>}/>
+            <Route path="/administrador" element={<Administrador></Administrador>}/>
+            
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Administrador.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Administrador = () => {
+    return (
+        <div>
+            AdminPage
+        </div>
+    );
+};
+
+export default Administrador;
