6015e6caef8b4764d60e098245acc9e1fa43fcfa
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-07-13 21:10:22 -0300

    agregue la table de usuarios y pedidos

---
+++ src/componentes/ItemUsuario.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import toretto from "../assets/toretto.jpeg";
+import { BsPauseCircleFill } from "react-icons/bs";
+
+const ItemUsuario = () => {
+  return (
+    <tr>
+      <td id="tdImg">
+        <figure>
+          <img src={toretto} alt="" />
+        </figure>
+      </td>
+      <td>admin@admin.com</td>
+      <td>Rolling</td>
+      <td>Administrador</td>
+      <td className="bg-transparent text-danger tdBtn">
+        <BsPauseCircleFill className="iconoPausar"></BsPauseCircleFill>
+      </td>
+    </tr>
+  );
+};
+
+export default ItemUsuario;
--- src/pages/Administrador.jsx
+++ src/pages/Administrador.jsx
@@ -5,6 +5,9 @@ import "./Administrador.css";
 import { Button, Container } from "react-bootstrap";
 import { BsFillPlusSquareFill, BsCheckSquareFill } from "react-icons/bs";
 import ItemProducto from "../componentes/ItemProducto";
+import Table from "react-bootstrap/Table";
+import ItemUsuario from "../componentes/ItemUsuario";
+import ItemPedidos from "../componentes/ItemPedidos";
 
 const Administrador = () => {
   return (
@@ -22,40 +25,80 @@ const Administrador = () => {
       </section>
       {/*Contenedor de administracion*/}
       <section className="text-white bg-dark contenedorAdministracion">
-        {/*Título y botones de agregar y seleccionar*/}
-        <article className="d-flex justify-content-between mb-5">
-          <h2>Productos</h2>
-          <div>
-            <Button variant="success" className="btnAgregar me-1">
-              Agregar
-            </Button>
-            <Button variant="warning" className="btnSeleccionar">
-              Seleccionar
-            </Button>
-            <BsFillPlusSquareFill className="iconoAgregar me-2"></BsFillPlusSquareFill>
-            <BsCheckSquareFill className="iconoSeleccionar"></BsCheckSquareFill>
+        <article>
+          {/*Título y botones de agregar y seleccionar*/}
+          <div className="d-flex justify-content-between mb-5">
+            <h2>Productos</h2>
+            <div>
+              <Button variant="success" className="btnAgregar me-1">
+                Agregar
+              </Button>
+              <Button variant="warning" className="btnSeleccionar">
+                Seleccionar
+              </Button>
+              <BsFillPlusSquareFill className="iconoAgregar me-2"></BsFillPlusSquareFill>
+              <BsCheckSquareFill className="iconoSeleccionar"></BsCheckSquareFill>
+            </div>
+          </div>
+          {/*Productos*/}
+          <div className="row row-cols-sm-1 row-cols-md-3 row-cols-lg-3 contenedorProductos">
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
+            <ItemProducto></ItemProducto>
           </div>
         </article>
-        {/*Productos*/}
-        <article className="row row-cols-sm-1 row-cols-md-3 row-cols-lg-3 contenedorProductos">
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-          <ItemProducto></ItemProducto>
-        </article>
         {/*Usuarios*/}
-        <article></article>
-        {/*Pedidos*/}
-        <article>
+        <article className="my-5">
+          <div className="d-flex justify-content-between mb-3">
             <h2>Usuarios</h2>
+            <div>
+              <Button variant="success" className="btnAgregar me-1">
+                Agregar
+              </Button>
+              <BsFillPlusSquareFill className="iconoAgregar me-2"></BsFillPlusSquareFill>
+            </div>
+          </div>
+          <Table responsive className="tabla">
+            <thead>
+              <tr>
+                <th>Img</th>
+                <th>Email</th>
+                <th>Nombre</th>
+                <th>Rol</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ItemUsuario></ItemUsuario>
+              <ItemUsuario></ItemUsuario>
+            </tbody>
+          </Table>
+        </article>
+        {/*Pedidos*/}
+        <article className="mb-5">
+          <h2 className="mb-3">Pedidos</h2>
+          <Table responsive className="tabla">
+            <thead>
+              <tr>
+                <th>Email de usuario</th>
+                <th>Nombre</th>
+                <th>Estado</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ItemPedidos></ItemPedidos>
+              <ItemPedidos></ItemPedidos>
+            </tbody>
+          </Table>
         </article>
       </section>
       <Footer></Footer>
