311501fa755eb1aae10291d281c93990d5a091ea
Aixa Filsinger <macafil99@gmail.com>
2023-07-13 21:39:39 -0300

    se acomodo el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-//import Header from "./componentes/comunes/Header";
+import Header from "./componentes/comunes/Header";
 
-//import QuienesSomos from "./componentes/comunes/QuienesSomos";
+import QuienesSomos from "./componentes/comunes/QuienesSomos";
 
-//import BuscadorPlatos from "../BuscadorPlatos";
-//import Footer from "./componentes/comunes/Footer";
-//import Menu from "./componentes/comunes/Menu";
-import DetalleProducto from "./componentes/vistas/DetalleProducto";
+import BuscadorPlatos from "../BuscadorPlatos";
+import Footer from "./componentes/comunes/Footer";
+import Menu from "./componentes/comunes/Menu";
 
 function App() {
-    return (
-        
-       /* <>
-            <Header />
-            <main>
-                <BuscadorPlatos />
-                <QuienesSomos />
-                <Menu /> 
-                
-            </main>
-            <Footer />
-        </>*/
-        <DetalleProducto></DetalleProducto>
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <BuscadorPlatos />
+        <QuienesSomos />
+        <Menu />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
