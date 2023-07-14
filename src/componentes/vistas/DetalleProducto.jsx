cdd8f139fbbb4526ae78d82240b59d1884b12101
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-13 21:49:28 -0300

    Se corrigieron errores en el css

--- src/componentes/vistas/DetalleProducto.jsx
+++ src/componentes/vistas/DetalleProducto.jsx
@@ -1,6 +1,6 @@
 import { Container, Card, Row, Col } from "react-bootstrap";
 import Footer from "../comunes/Footer";
-import hamburguesaDoble from "../../assets/Rectangle 110.png";
+import hamburguesaDoble from "../../complementos/imagenes/Rectangle 110.png";
 import { GoBookmark } from "react-icons/go";
 import { RiTruckLine } from "react-icons/ri";
 
