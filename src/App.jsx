05e7d998791721cb1ee68916bcf4326f0102035e
Aixa Filsinger <macafil99@gmail.com>
2023-07-15 03:58:58 -0300

    se ordeno el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,18 @@ import QuienesSomos from "./componentes/comunes/QuienesSomos";
 import BuscadorPlatos from "./componentes/BuscadorPlatos";
 import Footer from "./componentes/comunes/Footer";
 import Menu from "./componentes/comunes/Menu";
-import Administrador from "./pages/Administrador";
-import ContenedorCarrito from "./componentes/ContenedorCarrito";
-import CrearEditarProducto from "./componentes/CrearEditarProducto";
+
 
 function App() {
   return (
     <>
       <Header />
-      {/*<main>
+      <main>
         <BuscadorPlatos />
         <QuienesSomos />
         <Menu />
-        
-      </main>*/}
-      <CrearEditarProducto></CrearEditarProducto>
+      </main>
+      
       <Footer />
     </>
   );
