b40bb94dc7b2f005badd9735543c9746c9928a71
Juanchi <toranzos628@gmail.com>
2023-07-15 21:34:53 -0300

    agregue los conflictos que daba el componente inicio

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,24 @@ import QuienesSomos from "./componentes/comunes/QuienesSomos";
 import BuscadorPlatos from "./componentes/BuscadorPlatos";
 import Footer from "./componentes/comunes/Footer";
 import Menu from "./componentes/comunes/Menu";
-<<<<<<< HEAD
 import ContenedorCarrito from "./componentes/ContenedorCarrito";
 import ModalPago from "./componentes/ModalPago";
-
-function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <BuscadorPlatos />
-                <QuienesSomos />
-                <Menu />
-                <ModalPago />
-                {/* <ContenedorCarrito /> */}
-            </main>
-
-            <Footer />
-        </>
-    );
-=======
-import Administrador from "./pages/Administrador"
 import InicioSesion from "./componentes/InicioSesion";
 
 function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <BuscadorPlatos />
-        <QuienesSomos />
-        <Menu />
-        {/* <InicioSesion></InicioSesion> */}
-      </main>
-    <Footer />
-    </>
-  );
->>>>>>> iniciodesesion
-}
+      <>
+          <Header />
+          <main>
+              <BuscadorPlatos />
+              <QuienesSomos />
+              <Menu />
+              <ModalPago />
+              {/* <ContenedorCarrito /> */}
+              {/* <InicioSesion></InicioSesion> */}
+          </main>
 
-export default App;
+          <Footer />
+      </>
+  );}
+  export default App
