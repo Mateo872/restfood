918ad9b7f1ea30998ded1d894a01430e7016db3d
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-19 14:49:47 -0300

    Queries hechas y linkeado de db.json en el archivo env

--- src/componentes/ContenedorPlato.jsx
+++ src/componentes/ContenedorPlato.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import PlatoItem from "./PlatoItem";
+import { obtenerPlatos } from "./ayudas/consultas";
 
 const ContenedorPlato = () => {
+  const [platos, setPlatos] = useState([]);
+  // const []
+
+  useEffect(() => {
+    obtenerPlatos().then((res) => {
+      setPlatos(res[0].categorias);
+    });
+  }, []);
+
   return (
     <div className="contenedor_slider  position-absolute">
       <div className="contenedor_buscador d-flex flex-column justify-content-between gap-2">
