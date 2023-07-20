e19ab1dbd5843923483838c3a3de9adc718d0328
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-20 20:35:30 -0300

    Paginación de platos hecha

--- src/componentes/TarjetaProducto.jsx
+++ src/componentes/TarjetaProducto.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import { Card } from "react-bootstrap";
 import { BsPlusCircleFill } from "react-icons/bs";
 
-const TarjetaProducto = ({ platosFiltrados }) => {
+const TarjetaProducto = ({
+  platosFiltrados,
+  paginaActual,
+  productosPorPagina,
+}) => {
+  const indiceUltimoProducto = paginaActual * productosPorPagina;
+  const indicePrimerProducto = indiceUltimoProducto - productosPorPagina;
+  const productosPaginaActual = platosFiltrados.slice(
+    indicePrimerProducto,
+    indiceUltimoProducto
+  );
+
   return (
     <>
       {platosFiltrados
-        ? platosFiltrados.map((producto, index) => (
+        ? productosPaginaActual.map((producto, index) => (
             <Card key={index} className="card-body my-3 mt-5 mb-5">
               <Card.Img
                 variant="top"
@@ -41,33 +51,3 @@ const TarjetaProducto = ({ platosFiltrados }) => {
 };
 
 export default TarjetaProducto;
-/* 
-productos.map((producto, index) => (
-                      <Card key={index} className="card-body my-3 mt-5 mb-5">
-                          <Card.Img
-                              variant="top"
-                              src={producto.imagen}
-                              className="img-tarjeta-producto"
-                          />
-                          <Card.Body className="body-tarjeta">
-                              <Card.Title className="">{producto.nombre}</Card.Title>
-                              <Card.Text className="fs-5 my-4">
-                                  Stock:{" "}
-                                  <span className="fw-light text-secondary">
-                                      {" "}
-                                      {producto.stock}
-                                  </span>
-                              </Card.Text>
-                              <div className="d-flex mt-5 justify-content-between">
-                                  <Card.Text className="fs-5 my-5">
-                                      {" "}
-                                      $ {producto.precio}
-                                  </Card.Text>
-                                  <a>
-                                      <BsPlusCircleFill fontSize={40} className="mt-5" />
-                                  </a>
-                              </div>
-                          </Card.Body>
-                      </Card>
-                  ))
-                    */
--- src/componentes/comunes/Menu.jsx
+++ src/componentes/comunes/Menu.jsx
@@ -3,10 +3,14 @@ import { Container } from "react-bootstrap";
 import TarjetaProducto from "../TarjetaProducto";
 import { useForm } from "react-hook-form";
 import { obtenerPlatos } from "../ayudas/consultas";
+import Paginacion from "../Paginacion";
 
 const Menu = () => {
   const [busqueda, setBusqueda] = useState("");
   const [productos, setProductos] = useState([]);
+  const [paginaActual, setPaginaActual] = useState(1);
+  const productosPorPagina = 6;
+
   const {
     register,
     handleSubmit,
@@ -30,11 +34,14 @@ const Menu = () => {
 
   const manejoBuscador = (e) => {
     setBusqueda(e.target.value);
+    setPaginaActual(1);
   };
 
   const platosFiltrados = productos.filter((producto) =>
     producto.nombre.toLowerCase().includes(busqueda.toLowerCase())
   );
+
+  const totalPaginas = Math.ceil(platosFiltrados.length / productosPorPagina);
 
   return (
     <>
@@ -43,7 +50,7 @@ const Menu = () => {
         <Container className="menu-contenedor-body">
           <h2 className="text-white text-center menu-titulo mb-5">Menu</h2>
           <div className="d-flex flex-column flex-md-row flex-lg-row justify-content-between">
-            <p className="fs-3 text-center text-lg-start mb-0 ">
+            <p className="fs-3 text-center text-lg-start mb-2 mb-md-0">
               Busca tus productos
             </p>
             <input
@@ -56,8 +63,25 @@ const Menu = () => {
           </div>
           <hr className="text-white " />
           <section className="container-menu-card">
-            <div className="row justify-content-center justify-content-md-between gap-5 ">
-              <TarjetaProducto platosFiltrados={platosFiltrados} />
+            <div
+              className="row gap-5 "
+              style={{
+                justifyContent:
+                  platosFiltrados.length < 3 ? "center" : "space-between",
+              }}
+            >
+              <TarjetaProducto
+                platosFiltrados={platosFiltrados}
+                paginaActual={paginaActual}
+                productosPorPagina={productosPorPagina}
+              />
+            </div>
+            <div className="d-flex justify-content-center align-items-center pb-5">
+              <Paginacion
+                totalPaginas={totalPaginas}
+                paginaActual={paginaActual}
+                setPaginaActual={setPaginaActual}
+              />
             </div>
           </section>
         </Container>
