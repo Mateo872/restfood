fcf090bbb06aa4f59288fcfb1cf58bd0aed0bb33
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-20 19:11:06 -0300

    Cambios de estilos en las cards de productos y dinamismo de platos al buscar

--- src/componentes/TarjetaProducto.jsx
+++ src/componentes/TarjetaProducto.jsx
@@ -1,43 +1,43 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import hamburguesa from "../complementos/imagenes/img-hamb-card.png";
 import { BsPlusCircleFill } from "react-icons/bs";
 
-const TarjetaProducto = ({ productos, busqueda }) => {
-    return (
-        <>
-            {busqueda
-                ? busqueda.map((producto, index) => (
-                      <Card key={index} className="card-body my-3 mt-5 mb-5">
-                          <Card.Img
-                              variant="top"
-                              src={producto.imagen}
-                              className="img-tarjeta-producto"
-                          />
-                          <Card.Body className="body-tarjeta">
-                              <Card.Title className="fs-3">{producto.nombre}</Card.Title>
-                              <Card.Text className="fs-5 my-4">
-                                  Stock:{" "}
-                                  <span className="fw-light text-secondary">
-                                      {" "}
-                                      {producto.stock}
-                                  </span>
-                              </Card.Text>
-                              <div className="d-flex mt-5 justify-content-between">
-                                  <Card.Text className="fs-5 my-5">
-                                      {" "}
-                                      $ {producto.precio}
-                                  </Card.Text>
-                                  <a>
-                                      <BsPlusCircleFill fontSize={40} className="mt-5" />
-                                  </a>
-                              </div>
-                          </Card.Body>
-                      </Card>
-                  ))
-                : ""}
-        </>
-    );
+const TarjetaProducto = ({ platosFiltrados }) => {
+  return (
+    <>
+      {platosFiltrados
+        ? platosFiltrados.map((producto, index) => (
+            <Card key={index} className="card-body my-3 mt-5 mb-5">
+              <Card.Img
+                variant="top"
+                src={producto.imagen}
+                className="img-tarjeta-producto"
+                style={{ border: "6px solid #c7a17a" }}
+              />
+              <Card.Body className="body-tarjeta">
+                <Card.Title className="nombre_producto" title={producto.nombre}>
+                  {producto.nombre}
+                </Card.Title>
+                <Card.Text className="fs-5 my-4">
+                  Stock:{" "}
+                  <span className="fw-light text-secondary">
+                    {" "}
+                    {producto.stock}
+                  </span>
+                </Card.Text>
+                <div className="d-flex mt-5 justify-content-between align-items-center">
+                  <Card.Text className="fs-5 my-5">
+                    {" "}
+                    $ {producto.precio}
+                  </Card.Text>
+                  <BsPlusCircleFill fontSize={40} />
+                </div>
+              </Card.Body>
+            </Card>
+          ))
+        : ""}
+    </>
+  );
 };
 
 export default TarjetaProducto;
@@ -50,7 +50,7 @@ productos.map((producto, index) => (
                               className="img-tarjeta-producto"
                           />
                           <Card.Body className="body-tarjeta">
-                              <Card.Title className="fs-3">{producto.nombre}</Card.Title>
+                              <Card.Title className="">{producto.nombre}</Card.Title>
                               <Card.Text className="fs-5 my-4">
                                   Stock:{" "}
                                   <span className="fw-light text-secondary">
--- src/componentes/comunes/Menu.jsx
+++ src/componentes/comunes/Menu.jsx
@@ -1,94 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import TarjetaProducto from "../TarjetaProducto";
-import { BsHandIndexThumbFill } from "react-icons/bs";
 import { useForm } from "react-hook-form";
 import { obtenerPlatos } from "../ayudas/consultas";
 
 const Menu = () => {
-    const [busqueda, setBusqueda] = useState([]);
-    const [productos, setProductos] = useState([]);
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm();
+  const [busqueda, setBusqueda] = useState("");
+  const [productos, setProductos] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-    useEffect(() => {
-        obtenerPlatos().then((res) => {
-            const todasCategorias = [
-                ...res[0].categorias.entradas,
-                ...res[0].categorias.bebidas,
-                ...res[0].categorias.postres,
-                ...res[0].categorias.bebidasAlcoholicas,
-                ...res[0].categorias.comidasVeganas,
-            ];
+  useEffect(() => {
+    obtenerPlatos().then((res) => {
+      const todasCategorias = [
+        ...res[0].categorias.entradas,
+        ...res[0].categorias.bebidas,
+        ...res[0].categorias.postres,
+        ...res[0].categorias.bebidasAlcoholicas,
+        ...res[0].categorias.comidasVeganas,
+      ];
 
-            setProductos(todasCategorias);
-        });
-    }, []);
+      setProductos(todasCategorias);
+    });
+  }, []);
 
-    const onSubmit = (prod) => {
-        const platosFiltrados = productos.filter((producto) =>
-            producto.nombre.toLowerCase().includes(prod.producto.toLowerCase())
-        );
-        setBusqueda(platosFiltrados);
-        reset();
-    };
+  const manejoBuscador = (e) => {
+    setBusqueda(e.target.value);
+  };
 
-    return (
-        <>
-            <div className="menuConteiner"></div>
-            <section className="menu-contenedor" id="productos">
-                <Container className="menu-contenedor-body">
-                    <h2 className="text-white text-center menu-titulo mb-5">Menu</h2>
-                    <div className="d-flex flex-column flex-md-row flex-lg-row justify-content-between">
-                        <p className="fs-3 col col-lg-6 text-center text-lg-start  mb-5 ">
-                            Busca tus productos
-                        </p>
-                        <Form
-                            className="d-flex col col-lg-6 justify-content-lg-end justify-content-center align-self-center"
-                            onSubmit={handleSubmit(onSubmit)}
-                        >
-                            <Form.Group className="">
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Buscar"
-                                    {...register("producto", {
-                                        required:
-                                            "Este dato es requerido para poder buscar",
-                                        minLength: {
-                                            value: 2,
-                                            message:
-                                                "La busqueda debe tener al menos 2 caracteres",
-                                        },
-                                        maxLength: {
-                                            value: 20,
-                                            message:
-                                                "La busqueda debe tener como maximo 20 caracteres",
-                                        },
-                                    })}
-                                />
-                                <Form.Text className="text-danger">
-                                    {errors.producto?.message}
-                                </Form.Text>
-                            </Form.Group>
-                            <Button type="submit" className="boton-menu">
-                                <BsHandIndexThumbFill size={28} />
-                            </Button>
-                        </Form>
-                    </div>
-                    <hr className="text-white " />
-                    <section className="container-menu-card">
-                        <div className="row  justify-content-center gap-5 ">
-                            <TarjetaProducto productos={productos} busqueda={busqueda} />
-                        </div>
-                    </section>
-                </Container>
-            </section>
-        </>
-    );
+  const platosFiltrados = productos.filter((producto) =>
+    producto.nombre.toLowerCase().includes(busqueda.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="menuConteiner"></div>
+      <section className="menu-contenedor" id="productos">
+        <Container className="menu-contenedor-body">
+          <h2 className="text-white text-center menu-titulo mb-5">Menu</h2>
+          <div className="d-flex flex-column flex-md-row flex-lg-row justify-content-between">
+            <p className="fs-3 text-center text-lg-start mb-0 ">
+              Busca tus productos
+            </p>
+            <input
+              type="text"
+              placeholder="Busca tus platos"
+              onChange={manejoBuscador}
+              value={busqueda}
+              className="input_menu"
+            />
+          </div>
+          <hr className="text-white " />
+          <section className="container-menu-card">
+            <div className="row justify-content-center justify-content-md-between gap-5 ">
+              <TarjetaProducto platosFiltrados={platosFiltrados} />
+            </div>
+          </section>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Menu;
