189776efd75a7048ab371d3beb903b6dd76e96dd
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-21 03:04:44 -0300

    Animación al abrir el sobre hecha

---
+++ src/componentes/CardNosotros.jsx
@@ -0,0 +1,10 @@
+const CardNosotros = ({ nosotros }) => {
+  return (
+    <div className="d-flex flex-md-column text-center align-items-center gap-2">
+      <h1 className="nombre_nosotros">Mateo</h1>
+      <h3 className="edad_nosotros">20</h3>
+    </div>
+  );
+};
+
+export default CardNosotros;
--- src/componentes/Nosotros.jsx
+++ src/componentes/Nosotros.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { useState } from "react";
+import CardNosotros from "./CardNosotros";
 
 const Nosotros = () => {
   const [tarjeta, setTarjeta] = useState(false);
+  const nosotros = [
+    {
+      nombre: "Mateo",
+      edad: 21,
+    },
+    {
+      nombre: "Mario",
+      edad: 25,
+    },
+    ,
+    {
+      nombre: "Aixa",
+      edad: 22,
+    },
+    {
+      nombre: "Tadeo",
+      edad: 23,
+    },
+    {
+      nombre: "Juan",
+      edad: 22,
+    },
+  ];
   return (
     <section>
       <article
@@ -18,24 +42,22 @@ const Nosotros = () => {
               tarjeta && "contenedor_tarjetas-activo"
             }`}
           >
-            <div className="tarjetas_nosotros">
-              <p className="mb-0">Mateo</p>
-            </div>
-            <div className="tarjetas_nosotros">
-              <p className="mb-0">Mario</p>
-            </div>
-            <div className="tarjetas_nosotros">
-              <p className="mb-0">Aixa</p>
-            </div>
-            <div className="tarjetas_nosotros">
-              <p className="mb-0">Tadeo</p>
-            </div>
-            <div className="tarjetas_nosotros">
-              <p className="mb-0">Juan</p>
-            </div>
+            {nosotros.map((nos) => (
+              <div className="tarjetas_nosotros" key={nos.nombre}>
+                <p className="mb-0">{nos.nombre}</p>
+              </div>
+            ))}
           </div>
           <h4 className={`position-absolute ${tarjeta && "d-none"}`}>ABRIR</h4>
           <div className={`tarjeta ${tarjeta && "tarjeta_abierta"}`}></div>
+        </div>
+        <div
+          className={`${
+            tarjeta && "nosotros_carac-activo"
+          } nosotros_carac d-flex flex-column flex-md-row justify-content-center align-items-center mt-4`}
+        >
+          <div className="imagen_nosotros"></div>
+          <CardNosotros nosotros={nosotros} />
         </div>
       </article>
     </section>
