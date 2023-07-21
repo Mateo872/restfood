5d3371dc06f8bf5d7ee11dc58249efd98e6c6014
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-21 04:00:01 -0300

    Animaciones y estilos hechos en nosotros

--- src/componentes/CardNosotros.jsx
+++ src/componentes/CardNosotros.jsx
@@ -1,8 +1,10 @@
-const CardNosotros = ({ nosotros }) => {
+const CardNosotros = ({ nosotros, tarjetaNombre }) => {
+  const nombreFiltrado = nosotros.filter((nos) => nos.nombre === tarjetaNombre);
+
   return (
     <div className="d-flex flex-md-column text-center align-items-center gap-2">
-      <h1 className="nombre_nosotros">Mateo</h1>
-      <h3 className="edad_nosotros">20</h3>
+      <h1 className="nombre_nosotros">{nombreFiltrado[0]?.nombre}</h1>
+      <h3 className="edad_nosotros">{nombreFiltrado[0]?.edad}</h3>
     </div>
   );
 };
--- src/componentes/Nosotros.jsx
+++ src/componentes/Nosotros.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import CardNosotros from "./CardNosotros";
+import { useEffect } from "react";
 
 const Nosotros = () => {
   const [tarjeta, setTarjeta] = useState(false);
+  const [tarjetaNosotros, setTarjetaNosotros] = useState(false);
+  const [tarjetaNombre, setTarjetaNombre] = useState("");
+
   const nosotros = [
     {
       nombre: "Mateo",
@@ -13,7 +17,6 @@ const Nosotros = () => {
       nombre: "Mario",
       edad: 25,
     },
-    ,
     {
       nombre: "Aixa",
       edad: 22,
@@ -27,6 +30,16 @@ const Nosotros = () => {
       edad: 22,
     },
   ];
+
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      if (!e.target.className.includes("tarjeta")) {
+        setTarjeta(false);
+        setTarjetaNosotros(false);
+      }
+    });
+  }, []);
+
   return (
     <section>
       <article
@@ -35,7 +48,15 @@ const Nosotros = () => {
       >
         <div
           className="contenedor_tarjeta d-flex justify-content-center"
-          onClick={() => setTarjeta(!tarjeta)}
+          onClick={(e) => {
+            setTarjeta(!tarjeta);
+            if (
+              e.target.className.includes("sobre") ||
+              e.target.className.includes("titulo_abrir")
+            ) {
+              setTarjetaNosotros(false);
+            }
+          }}
         >
           <div
             className={`contenedor_tarjetas d-flex justify-content-center justify-content-md-between position-absolute ${
@@ -43,21 +64,37 @@ const Nosotros = () => {
             }`}
           >
             {nosotros.map((nos) => (
-              <div className="tarjetas_nosotros" key={nos.nombre}>
-                <p className="mb-0">{nos.nombre}</p>
-              </div>
+              <p
+                className="tarjetas_nosotros"
+                key={nos.nombre}
+                onClick={() => {
+                  setTarjetaNosotros(true);
+                  setTarjetaNombre(nos.nombre);
+                }}
+              >
+                {nos.nombre}
+              </p>
             ))}
           </div>
-          <h4 className={`position-absolute ${tarjeta && "d-none"}`}>ABRIR</h4>
-          <div className={`tarjeta ${tarjeta && "tarjeta_abierta"}`}></div>
+          <h4
+            className={`titulo_abrir position-absolute ${tarjeta && "d-none"}`}
+          >
+            ABRIR
+          </h4>
+          <div
+            className={`sobre tarjeta ${tarjeta && "tarjeta_abierta"}`}
+          ></div>
         </div>
         <div
           className={`${
-            tarjeta && "nosotros_carac-activo"
+            tarjetaNosotros && "nosotros_carac-activo"
           } nosotros_carac d-flex flex-column flex-md-row justify-content-center align-items-center mt-4`}
         >
-          <div className="imagen_nosotros"></div>
-          <CardNosotros nosotros={nosotros} />
+          <div
+            className="imagen_nosotros"
+            style={{ backgroundColor: "#ddd" }}
+          ></div>
+          <CardNosotros nosotros={nosotros} tarjetaNombre={tarjetaNombre} />
         </div>
       </article>
     </section>
