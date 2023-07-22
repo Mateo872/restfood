85f57a01b2f1c8483bd194acabb5b504ff501968
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-22 02:16:53 -0300

    Estilos para tablet hecho

--- src/componentes/DetalleProducto.jsx
+++ src/componentes/DetalleProducto.jsx
@@ -17,53 +17,56 @@ const DetalleProducto = () => {
   return (
     <section>
       <article className="contenedor_detalle">
-        <div className="d-flex gap-1">
+        <div className="d-flex gap-1 paginacion">
           <Link to={"/"}>INICIO /</Link>
           <span className="paginacion_detalle-color">HAMBURGUESA DOBLE</span>
         </div>
         <p className="stock mb-0">
           Stock - <span>20</span>
         </p>
-        <div className="d-flex flex-column flex-md-row">
+        <div className="contenedor_imagen-carac d-flex flex-column flex-md-row gap-4">
           <div className="contenedor_imagen-detalle">
             <img className="w-100 h-100" src={hambur} alt="" />
           </div>
-          <h2 className="titulo_producto-detalle">HAMBURGUESA DOBLE</h2>
-          <h2 className="mt-0">$1000</h2>
-          <p className="descripcion_detalle mb-0">
-            Una deliciosa creación culinaria compuesta por dos jugosas y
-            sabrosas hamburguesas de carne, apiladas una encima de la otra, y
-            separadas por capas de queso fundido. Entre los panes suaves y
-            ligeramente tostados se encuentran también una variedad de aderezos
-            y condimentos, como lechuga crujiente, tomate fresco, cebolla,
-            pepinillos y una irresistible salsa.
-          </p>
-          <hr />
-          <h4>Tamaño</h4>
-          <div className="d-flex flex-column flex-md-row gap-2 align-items-center">
-            <p className="contenedor_tamanio contenedor_tamanio-activo">
-              Chico
+          <div className="contenedor_caracteristicas-detalle d-flex flex-column">
+            <h2 className="titulo_producto-detalle">HAMBURGUESA DOBLE</h2>
+            <h2 className="titulo_precio-detalle mt-0">$1000</h2>
+            <p className="descripcion_detalle mb-0" title="">
+              Una deliciosa creación culinaria compuesta por dos jugosas y
+              sabrosas hamburguesas de carne, apiladas una encima de la otra, y
+              separadas por capas de queso fundido. Entre los panes suaves y
+              ligeramente tostados se encuentran también una variedad de
+              aderezos y condimentos, como lechuga crujiente, tomate fresco,
+              cebolla, pepinillos y una irresistible salsa.
             </p>
-            <p className="contenedor_tamanio">Mediano</p>
-            <p className="contenedor_tamanio">Grande</p>
+            <h4 className="titulo_tamaño">Tamaño</h4>
+            <div className="d-flex flex-column flex-md-row gap-2 align-items-center">
+              <p className="contenedor_tamanio contenedor_tamanio-activo">
+                Chico
+              </p>
+              <p className="contenedor_tamanio">Mediano</p>
+              <p className="contenedor_tamanio">Grande</p>
+            </div>
+            <hr />
+            <h4>Cantidad</h4>
+            <input type="number" className="input_cantidad" placeholder="0" />
+            <hr />
+            <div className="d-flex align-items-center gap-1 mb-2">
+              <BsTruck className="svg_postal" size={25} />
+              <h4 className="mb-0">Medios de envío</h4>
+            </div>
+            <div className="d-flex gap-2 w-100">
+              <input
+                type="number"
+                className="input_postal"
+                placeholder="Tu código postal"
+              />
+              <button className="boton_calcular">Calcular</button>
+            </div>
+            <button className="agregar_carrito w-100">
+              Agregar al carrito
+            </button>
           </div>
-          <hr />
-          <h4>Cantidad</h4>
-          <input type="number" className="input_cantidad" placeholder="0" />
-          <hr />
-          <div className="d-flex align-items-center gap-1 mb-2">
-            <BsTruck className="svg_postal" size={25} />
-            <h4 className="mb-0">Medios de envío</h4>
-          </div>
-          <div className="d-flex gap-2 w-100">
-            <input
-              type="number"
-              className="input_postal"
-              placeholder="Tu código postal"
-            />
-            <button className="boton_calcular">Calcular</button>
-          </div>
-          <button className="agregar_carrito w-100">Agregar al carrito</button>
         </div>
       </article>
     </section>
