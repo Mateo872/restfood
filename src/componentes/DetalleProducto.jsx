657366f0e4f682b482ff3e1e7548b17f5017c6ed
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-22 01:24:36 -0300

    Estilos y estructura de la página de detalle hecho

--- src/componentes/DetalleProducto.jsx
+++ src/componentes/DetalleProducto.jsx
@@ -1,84 +1,72 @@
-import { Container, Card, Row, Col } from "react-bootstrap";
-import Footer from "./comunes/Footer";
-import { GoBookmark } from "react-icons/go";
-import { RiTruckLine } from "react-icons/ri";
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {obtenerPlato} from "./ayudas/consultas";
+import { Link, useParams } from "react-router-dom";
+import { GoBookmark } from "react-icons/go";
+import { BsTruck } from "react-icons/bs";
+import { obtenerPlato } from "./ayudas/consultas";
+import hambur from "../complementos/imagenes/hambur.png";
 
 const DetalleProducto = () => {
   const { id } = useParams();
   const [plato, setPlato] = useState([]);
 
-  useEffect(()=>{
-    obtenerPlato(id).then((respuesta)=>{
+  useEffect(() => {
+    obtenerPlato(id).then((respuesta) => {
       setPlato(respuesta);
     });
-  },[])
+  }, []);
   return (
-    <>
-      <Container className="my-5 mainDetalle">
-        <Row>
-          <h5 className="text-secondary">{plato.stock}</h5>
-          
-
-          <Col md={6}><div className="iconFav">
-            <GoBookmark className="display-1 text-dark"></GoBookmark>
+    <section>
+      <article className="contenedor_detalle">
+        <div className="d-flex gap-1">
+          <Link to={"/"}>INICIO /</Link>
+          <span className="paginacion_detalle-color">HAMBURGUESA DOBLE</span>
+        </div>
+        <p className="stock mb-0">
+          Stock - <span>20</span>
+        </p>
+        <div className="d-flex flex-column flex-md-row">
+          <div className="contenedor_imagen-detalle">
+            <img className="w-100 h-100" src={hambur} alt="" />
           </div>
-            <Card.Img variant="top" src={plato.imagen}  className="img-fluid imagendetalle"/>
-          </Col>
-          <Col md={6}>
-            <article className="mb-5">
-              <h2>{plato.nombre}</h2>
-              <h3 className="fw-bold">${plato.precio}</h3>
-              <hr />
-              <p>
-              {plato.descripcion}
-              </p>
-              <hr />
-              <h2>Tamaño</h2>
-              <div className="row">
-                <aside className="col-sm-4 col-md-3 mb-3">
-                  <button className="btn_DP ">Chico</button>
-                </aside>
-                <aside className="col-sm-4 col-md-3 mb-3">
-                  <button className="btn_DP ">Mediano</button>
-                </aside>
-                <aside className="col-sm-4 col-md-3 mb-3">
-                  {" "}
-                  <button className="btn_DP">Grande</button>
-                </aside>
-              </div>
-
-              <hr />
-              <h2>Cantidad</h2>
-              <input
-                type="text"
-                disabled
-                className="input_CantidadDP text-center"
-                placeholder="0"
-              />
-              <hr />
-              <h2>
-                <RiTruckLine className="fs-1" /> Medios de envíos
-              </h2>
-              <input
-                type="text"
-                placeholder="Tu código postal"
-                className="me-3 input_CantidadDP mb-3"
-              />
-              <button className="btn_CalcularDP">Calcular</button>
-            </article>
-            <div className="text-start">
-              <button type="submit" className="btn_AgrCarritoDP">
-                Agregar al carrito
-              </button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Footer></Footer>
-    </>
+          <h2 className="titulo_producto-detalle">HAMBURGUESA DOBLE</h2>
+          <h2 className="mt-0">$1000</h2>
+          <p className="descripcion_detalle mb-0">
+            Una deliciosa creación culinaria compuesta por dos jugosas y
+            sabrosas hamburguesas de carne, apiladas una encima de la otra, y
+            separadas por capas de queso fundido. Entre los panes suaves y
+            ligeramente tostados se encuentran también una variedad de aderezos
+            y condimentos, como lechuga crujiente, tomate fresco, cebolla,
+            pepinillos y una irresistible salsa.
+          </p>
+          <hr />
+          <h4>Tamaño</h4>
+          <div className="d-flex flex-column flex-md-row gap-2 align-items-center">
+            <p className="contenedor_tamanio contenedor_tamanio-activo">
+              Chico
+            </p>
+            <p className="contenedor_tamanio">Mediano</p>
+            <p className="contenedor_tamanio">Grande</p>
+          </div>
+          <hr />
+          <h4>Cantidad</h4>
+          <input type="number" className="input_cantidad" placeholder="0" />
+          <hr />
+          <div className="d-flex align-items-center gap-1 mb-2">
+            <BsTruck className="svg_postal" size={25} />
+            <h4 className="mb-0">Medios de envío</h4>
+          </div>
+          <div className="d-flex gap-2 w-100">
+            <input
+              type="number"
+              className="input_postal"
+              placeholder="Tu código postal"
+            />
+            <button className="boton_calcular">Calcular</button>
+          </div>
+          <button className="agregar_carrito w-100">Agregar al carrito</button>
+        </div>
+      </article>
+    </section>
   );
 };
 
