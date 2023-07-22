0c31b0558b31a78aa856652def71b45702085479
Mateo Bellini <bellinimateo872@gmail.com>
2023-07-22 04:38:49 -0300

    Cantidad y codigo postal validado

--- src/componentes/DetalleProducto.jsx
+++ src/componentes/DetalleProducto.jsx
@@ -4,13 +4,25 @@ import { GoBookmark, GoBookmarkFill } from "react-icons/go";
 import { BsTruck } from "react-icons/bs";
 import { obtenerPlato } from "./ayudas/consultas";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useRef } from "react";
 
 const DetalleProducto = () => {
   const { id } = useParams();
   const [plato, setPlato] = useState([]);
   const [favorito, setFavorito] = useState(false);
+  const [postal, setPostal] = useState(false);
+  const [mostrarSpinner, setMostrarSpinner] = useState(false);
   const [tamanio, setTamanio] = useState("Chico");
+  const codigoPostal = useRef(null);
   const navegacion = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     obtenerPlato(id)
@@ -24,6 +36,14 @@ const DetalleProducto = () => {
         console.log(err);
       });
   }, []);
+
+  const manejoEnvio = (data) => {
+    setMostrarSpinner(true);
+    setTimeout(() => {
+      setPostal(true);
+      setMostrarSpinner(false);
+    }, 1500);
+  };
 
   const manejoTamanio = (e) => {
     const nuevoTamanio = e.target.textContent;
@@ -73,7 +93,10 @@ const DetalleProducto = () => {
                   <GoBookmark className="bookmark position-absolute" />
                 )}
               </div>
-              <div className="contenedor_caracteristicas-detalle d-flex flex-column justify-content-lg-between">
+              <form
+                onSubmit={handleSubmit(manejoEnvio)}
+                className="contenedor_caracteristicas-detalle d-flex flex-column justify-content-lg-between"
+              >
                 <h2 className="titulo_producto-detalle mt-0">{plato.nombre}</h2>
                 <h2 className="titulo_precio-detalle mt-0">
                   ${obtenerPrecioConTamanio()}
@@ -114,24 +137,67 @@ const DetalleProducto = () => {
                   type="number"
                   className="input_cantidad"
                   placeholder="0"
+                  {...register("cantidad", {
+                    required: "La cantidad es obligatoria",
+                    pattern: {
+                      value: /^[0-9]+$/,
+                      message: "La cantidad debe ser un número válido",
+                    },
+                    min: {
+                      value: 1,
+                      message: "La cantidad debe ser mayor que cero",
+                    },
+                  })}
                 />
+                <div className="text-danger">{errors.cantidad?.message}</div>
                 <hr />
                 <div className="d-flex align-items-center gap-1 mb-2">
                   <BsTruck className="svg_postal" size={25} />
-                  <h4 className="mb-0">Medios de envío</h4>
+                  <h4 className="mb-0">Envío</h4>
                 </div>
-                <div className="d-flex gap-2 w-100">
-                  <input
-                    type="number"
-                    className="input_postal"
-                    placeholder="Tu código postal"
-                  />
-                  <button className="boton_calcular">Calcular</button>
+                {!postal ? (
+                  <div className="d-flex gap-2 w-100">
+                    <input
+                      type="text"
+                      className="input_postal"
+                      placeholder="Tu código postal"
+                      {...register("codigoPostal", {
+                        required: "El código postal es obligatorio",
+                        pattern: {
+                          value: /^[A-Z][0-9]{4,6}$/i,
+                          message:
+                            "El código postal debe tener un formato válido",
+                        },
+                      })}
+                    />
+                    {mostrarSpinner ? (
+                      <div className="d-flex justify-content-center align-items-center ms-2 w-25">
+                        <ClipLoader />
+                      </div>
+                    ) : (
+                      <button className="boton_calcular">Calcular</button>
+                    )}
+                  </div>
+                ) : (
+                  <div className="bg-success w-100 py-2 text-center text-light position-relative">
+                    Tu envío es de $300
+                    <span
+                      className="cerrar_postal position-absolute"
+                      onClick={() => {
+                        setPostal(false);
+                      }}
+                    >
+                      X
+                    </span>
+                  </div>
+                )}
+                <div className="text-danger w-75">
+                  {errors.codigoPostal?.message}
                 </div>
                 <button className="agregar_carrito w-100">
                   Agregar al carrito
                 </button>
-              </div>
+              </form>
             </div>
           </>
         ) : (
